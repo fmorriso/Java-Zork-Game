f66fbcba3d9f74f0c545d8526d731ae13283a339
Fred Morrison <frederick.morrison@outlook.com>
2025-05-15 20:38:07 -0600

    Add display name to Commands enum

--- Commands.java
+++ Commands.java
@@ -1,9 +1,20 @@
 public enum Commands {
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN,
-    GRAB,
-    FIGHT,
-    HELP;
+    LEFT("Left"),
+    RIGHT("Right"),
+    UP("Up"),
+    DOWN("Down"),
+    GRAB("Grab"),
+    FIGHT("Fight"),
+    HELP("Help"),;
+
+    private final String displayName;
+
+    Commands(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
