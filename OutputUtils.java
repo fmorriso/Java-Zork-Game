e78c74bf0dd8c2ae1ef3d03fa0ab6fd3eb9bfbcb
Fred Morrison <frederick.morrison@outlook.com>
2025-05-16 06:30:15 -0600

    add displayInformation to OutputUtils

--- OutputUtils.java
+++ OutputUtils.java
@@ -3,7 +3,8 @@ import javax.swing.JOptionPane;
 public class OutputUtils
 {
     /**
-     * Displays the specified message in a popup window with the specified title
+     * Displays the specified message in a popup window with the specified title.
+     * It is assumed that the message type is informational.
      *
      * @param msg   - the message to display
      * @param title - the title of the message dialog
@@ -11,6 +12,17 @@ public class OutputUtils
     public static void displayMessage(String msg, String title)
     {
         JOptionPane.showMessageDialog(null, msg, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    /**
+     * Displays the specified information message in a popup window with the specified title
+     *
+     * @param msg   - the message to display
+     * @param title - the title of the message dialog
+     */
+    public static void displayInformation(String msg, String title)
+    {
+        displayMessage(msg, title);
     }
 
     public static void displayWarning(String msg, String title)
